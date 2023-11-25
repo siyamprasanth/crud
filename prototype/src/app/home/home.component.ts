0ea18ccd4179d8c1df07e338084310e7c66ca7fa
siyamprasanth <siyamworkmail@gmail.com>
2023-11-25 11:36:57 +0530

    25/11/2023-11.36am

--- prototype/src/app/home/home.component.ts
+++ prototype/src/app/home/home.component.ts
@@ -8,21 +8,33 @@ import { FormBuilder, FormGroup,Validators } from '@angular/forms';
 })
 export class HomeComponent implements OnInit{
 userDetails: FormGroup;
+dataList:any;
 
-
-constructor(fb: FormBuilder){
-
+constructor(private fb: FormBuilder){
+  this.dataList =[];
 
   this.userDetails = fb.group({
     name:['',Validators.required],
     gender :['',Validators.required],
+    dob:['',Validators.required],
     mobileNumber:['',Validators.required],
     eMail: ['',Validators.required],
 
   })
 }
-saveData(){
-  
+public addItem(): void{
+  this.dataList.push(this.userDetails.value);
+  this.userDetails.reset();
+}
+reset(){
+  this.userDetails.reset()
+}
+
+removeItem(){
+//   this.dataList.forEach(value, index => {
+//     if (value == element)
+//     this.dataList.splice(index, 1)
+//   });
 }
 ngOnInit(): void {
   
